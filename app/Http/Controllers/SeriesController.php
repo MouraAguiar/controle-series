221c16c707e01686c7007f18d785e16172e5a4c5
S1 Moura Aguiar <mouraaguiarfma@dacta1.intraer>
2023-04-17 10:45:28 -0300

    first commit

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SeriesFormRequest;
+use App\Models\Episode;
+use App\Models\Season;
+use App\Models\Series;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class SeriesController extends Controller
+{
+    public function index(Request $request)
+
+    {
+      $series = Series::all();
+      $mensagemSucesso = session('mensagem.sucesso');
+
+
+      return view('series.index')->with('series', $series)
+          ->with('mensagemSucesso', $mensagemSucesso);
+    }
+
+    public function create()
+    {
+        return view('series.create');
+    }
+
+    public function store(SeriesFormRequest $request)
+    {
+        $series = Series::create($request->all());
+        $seasons = [];
+
+       for ($i = 1; $i <= $request->seasonsQty; $i++) {
+          $seasons[]= [
+              'series_id'=> $series->id,
+               'number'=> $i,
+              ];
+           }
+        Season::insert($seasons);
+
+
+       $episodes = [];
+       foreach ($series->seasons as $season) {
+
+           for ($j = 1; $j < $request->episodesPerSeason; $j++) {
+
+               $episodes[] = [
+                   'season_id' => $season->id,
+                   'number' => $j
+               ];
+
+           }
+
+           Episode::insert($episodes);
+
+           return to_route('series.index')
+               ->with('mensagem.sucesso', "Series '{$series->nome}' adicionada com sucesso");
+       }
+
+    }
+public function destroy(Series $series, Request $request)
+{
+        $series->delete();
+
+      return to_route('series.index')
+          ->with('mensagem.sucesso', "Series '{$series->nome}' removida com sucesso");
+
+
+}
+
+    public function edit(Series $series)
+    {
+        return view('series.edit')->with('serie', $series);
+}
+
+    public function update(Series $series, SeriesFormRequest $request)
+    {
+        $series->nome = $request-> nome;
+        $series->save();
+
+        return to_route('series.index')
+            ->with('mensagem.sucesso',"Série '{$series->nome}' atualizada com sucesso");
+}
+}
